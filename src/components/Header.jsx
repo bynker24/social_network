9a01cfcccdff0a2e5f03f6cbea936f83cc4f5d61
@bynker24 <puschkin.kir@yandex.ua>
2022-02-15 13:43:06 +0300

    15.02.22

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Header  = () => {
+    return <header className='header'>
+        <img src='https://www.pngkey.com/png/full/222-2224685_react-logo-png-transparent-react-logo.png' />
+    </header>
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Profile = () => {
+    return <div className='content'>
+        <img src='https://s1.1zoom.ru/b5050/612/Sunrises_and_sunsets_Forests_Scenery_Grass_Rays_of_587227_3840x2160.jpg' />
+
+        <a>Main content</a>
+    </div>
+}
+
+export default Profile;
