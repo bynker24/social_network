94d5b06fcaf15fd274dd9db550110aa1a458f0b0
@bynker24 <puschkin.kir@yandex.ua>
2022-02-16 16:49:30 +0300

    cssFail_16.02.22

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -5,6 +5,21 @@ const Profile = () => {
         <img src='https://s1.1zoom.ru/b5050/612/Sunrises_and_sunsets_Forests_Scenery_Grass_Rays_of_587227_3840x2160.jpg' />
 
         <a>Main content</a>
+
+        <div className="AvaDescription">
+
+        </div>
+
+        <div className="MyPosts">
+
+        </div>
+
+        <div className="NewPost">
+
+        </div>
+
+        
+
     </div>
 }
 
