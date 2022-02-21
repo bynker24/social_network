ec04504d4fe65f43fc39184a347602e6035a1181
@bynker24 <puschkin.kir@yandex.ua>
2022-02-21 10:03:34 +0300

    cr_compon_Me.Mu.N.S.

---
+++ src/components/Messeges/Messages.jsx
@@ -0,0 +1,23 @@
+import React from "react"
+import s from './Messeges.module.css'
+
+const Messeges = (props) => {
+    return (
+        <div className={s.messeges}>
+            <li>Messeges</li>
+            <li>Messeges</li>
+            <li>Messeges</li>
+            <li>Messeges</li>
+            <li>Messeges</li>
+            <li>Messeges</li>
+            <li>Messeges</li>
+            <li>Messeges</li>
+            <li>Messeges</li>
+            <li>Messeges</li>
+            <li>Messeges</li>
+            <li>Messeges</li>
+
+        </div>
+    )
+}
+export default Messeges;
